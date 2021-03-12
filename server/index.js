a32b3fce4956905b4c2f457ea0ca732cd69f2059
Emira Hajj <emirahajj@gmail.com>
2021-03-12 01:10:50 -0500

    solved findOneAndUpdate/updateOne duplication problems

--- server/index.js
+++ server/index.js
@@ -14,13 +14,11 @@ app.use(cors());
 //the conncetion URL we got from Cloud Atlas. 
 //will store them in environment variable when we deploy
 //**need to replace <password> with the password */
-const CONNECTION_URL = 'mongodb+srv://emira499:<password>.9fd8x.mongodb.net/billboard?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://emira499:<password>@cluster0.9fd8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 //we're going to use mongodb atlas
 const PORT = process.env.port || 5000;
 
-const connectionparams = {
-    useNewUrlParser: true, useUnifiedTopology: true
-}
+const connectionparams = {useNewUrlParser: true, useUnifiedTopology: true}
 
 const connection = mongoose.connect(CONNECTION_URL, connectionparams)
 .then(() => {
@@ -50,276 +48,38 @@ const Album = mongoose.model("album", albumSchema);
 const artistSchema = new mongoose.Schema({
     name: String,
     genres: {type: Array, "default": []},
-    albums: [albumSchema]
+    albums: [{type: mongoose.Schema.Types.ObjectId, ref: 'Album'}]
 });
 
 artistSchema.index({name: 1}, {unique: true})
 const Artist = mongoose.model("artist", artistSchema)
 
 const chartSchema = new mongoose.Schema({
-    2020: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2019: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2018: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2017: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2016: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2015: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2014: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2013: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2012: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2011: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2010: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2009: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2008: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2007: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2006: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2005: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2004: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2003: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2002: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2001: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  2000: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1999: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1998: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1997: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1996: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1995: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1994: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1993: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1992: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1991: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1990: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1989: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1988: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1987: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1986: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1985: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1984: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1983: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1982: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1981: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1980: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1979: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1978: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1977: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1976: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1975: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1974: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1973: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1972: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1971: [{
-        rank: Number,
-        album: albumSchema
-    }],
-  1970: [{
-        rank: Number,
-        album: albumSchema
-    }]
+  year: Number,
+  rank: Number,
+  album: {type: mongoose.Schema.Types.ObjectId, ref: 'Album'}
 });
 const Chart = mongoose.model("charts", chartSchema);
 
 
-tester.forEach(element => {
+tester.forEach(async element => {
     //first make the album object
-    //let id = new mongoose.Types.ObjectId();
+    let id = new mongoose.Types.ObjectId();
     let a = element.duration.split(":");
     var number = a.length > 2 ? parseInt(a[0])*60*60 + parseInt(a[1])*60 + parseInt(a[2]) : (parseInt(a[0])*60+ parseInt(a[1]));
     let b = parseInt(element.release_year);
     let newChartPos = {year: element.chart_year, rank: element.rank};
 
-    var album = new Album({
-        //_id: id,
-        title: element.title,
-        artist: element.artist,
-        img: element.image,
-        genre: element.genre,
-        duration: number,
-        release: b,
-        styles: element.subgenres,
-        chart_positions: {
-            year: element.chart_year,
-            rank: element.rank
-        },
-        awards: [element.awards]
+    await Album.findOneAndUpdate({title: element.title, artist: element.artist}, {$addToSet: {chart_positions: newChartPos}, img: element.image, duration: number}, {upsert: true, new: true}, (err, doc, res)=>{
+      id = doc._id; 
+      const chartEntry = new Chart({
+        year: element.chart_year,
+        rank: element.rank,
+        album: doc._id
+      })
+       chartEntry.save();
+      
     });
+     await Artist.updateOne({name: element.artist}, {$addToSet: {genres: element.genre, albums: id}}, {upsert: true, new: true});
 
-    //if this album exists, album will equal this incoming object, otherwise, album = what was found form the query
-    //Album.findOne({title: element.title, artist: element.artist}, function(err, docs) {
-    const artist = new Artist({
-        name: element.artist,
-        genres: [element.genre], 
-        albums: [album]
-    });
-
-
-     Album.updateOne({title: element.title, artist: element.artist}, {$push: {chart_positions: newChartPos}, img: element.image, duration: number}, {upsert: true}, function(err, result){
-        //  if (!err){
-        //     album = result;
-        //      if (!result){
-                
-        //      }
-        //  }
-     });
 });
-
-var testarray = [];
-var query = Album.find();
-query.exec((err, albums) => {
-    albums.forEach((entry) =>{
-        let album = entry.toObject();
-        let artist = album.artist;
-        let genre = album.genre;
-        Artist.updateOne({name: artist}, {$push: {genres: genre, albums: album}}, {upsert: true}, function(err, restul){
-
-        })
-    })
-});
-
-
-//console.log(testarray)
