0ea9e42cd5f26ed987489c3728fb3951615f7dbc
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-03-15 19:14:58 -0400

    Remove unnecessary files and code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="text-red-500">
-          If my text is red, tailwind is configured!
+        <p className="text-blue-700">
+          Hello in blue text
         </p>
       </header>
     </div>
