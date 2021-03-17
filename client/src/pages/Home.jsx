353f1485e723728663f5d3aafe52fcaef8780984
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-03-17 02:45:50 -0400

    Add home.jsx component

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="flex pt-10 justify-center text-indigo-400 font-bold text-3xl">Home Page</h1>
+        </div>
+    )
+}
+
+export default Home
