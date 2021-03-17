27ac4b2aa345df483f5eec4ce319a3fcc6413014
Emira Hajj <emirahajj@gmail.com>
2021-03-16 22:25:40 -0400

    pulled in data from DB as JSON

--- server/routes/albums.js
+++ server/routes/albums.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import Album from '../models/Albums.js'
+
 
 const router = express.Router();
 
-router.get("/",  (req,res)=> {
-    res.send("Albums home page goes here");
+router.get("/", async (req,res)=> {
+    const json =  await Album.find().limit(10)
+    .then((data) => {
+        res.json(data)
+    })
+    .catch((error)=> {
+        console.log('Error: ', error)
+    });
+    ;
 });
 
 export default router;
