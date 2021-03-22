f00ef225a1e262295c458bf9b930d32102ad695f
AlfredoMora06 <agmmendez123@gmail.com>
2021-03-22 12:50:30 -0400

    fixed Home typo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,22 @@
 import Navbar from "../components/Navbar"
-import fetchData from './api/index';
-import React, {useState, useEffect} from 'react';
+import fetchData from '../api/index';
+import React, { useState, useEffect } from 'react';
 import { fetchChart } from "../api";
 
 const Home = () => {
-    const [yearChart, setYearChart] = useState(2020);
+  const [yearChart, setYearChart] = useState(2020);
 
-    useEffect(()=> {
-      let albums = fetchChart().then((result) => {
-          //add more specific logic here like how many to return etc.
-        setYearChart(result.data);
-      });
-    })
-    return (
-        <div className="bg-indigo-600">
-            <Navbar />
-        </div>
-    )
+  useEffect(() => {
+    let albums = fetchChart().then((result) => {
+      //add more specific logic here like how many to return etc.
+      setYearChart(result.data);
+    });
+  })
+  return (
+    <div className="bg-indigo-600">
+      <Navbar />
+    </div>
+  )
 }
 
 export default Home
