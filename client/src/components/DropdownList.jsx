15374b4b046bc727fc984a83aa3078b5a306ee97
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-04-01 16:39:18 -0400

    Re-commit last commit and remove .env for now

--- client/src/components/DropdownList.jsx
+++ client/src/components/DropdownList.jsx
@@ -2,13 +2,21 @@ import DropdownItem from "../components/DropdownItem"
 
 const DropdownList = (props) => {
     return (
-        <div className="absolute mt-4 py-2 w-44 bg-dark rounded-2xl shadow-xl z-20 font-bold text-center justify-center">
+        <div className="absolute mt-4 py-2 w-44 bg-dark rounded-2xl shadow-xl font-bold text-center justify-center">
             <DropdownItem onSwitch={props.onSwitch} text={2020}/>
             <DropdownItem onSwitch={props.onSwitch} text={2019}/>
             <DropdownItem onSwitch={props.onSwitch} text={2018}/>
             <DropdownItem onSwitch={props.onSwitch} text={2017}/>
             <DropdownItem onSwitch={props.onSwitch} text={2016}/>
             <DropdownItem onSwitch={props.onSwitch} text={2015}/>
+            <DropdownItem onSwitch={props.onSwitch} text={2014}/>
+            <DropdownItem onSwitch={props.onSwitch} text={2013}/>
+            <DropdownItem onSwitch={props.onSwitch} text={2012}/>
+            <DropdownItem onSwitch={props.onSwitch} text={2011}/>
+            <DropdownItem onSwitch={props.onSwitch} text={2010}/>
+            <DropdownItem onSwitch={props.onSwitch} text={2009}/>
+            <DropdownItem onSwitch={props.onSwitch} text={2008}/>
+            <DropdownItem onSwitch={props.onSwitch} text={2007}/>
         </div>
     )
 }
