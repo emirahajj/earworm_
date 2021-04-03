f5cf7c6f2545e8386f0adb256d3cf84c83b7744c
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-04-03 17:27:48 -0400

    Add years up to 1970 and a scroll bar for the dropdown list

--- client/src/components/DropdownList.jsx
+++ client/src/components/DropdownList.jsx
@@ -1,22 +1,23 @@
 import DropdownItem from "../components/DropdownItem"
 
 const DropdownList = (props) => {
+
+    var years = []
+    
+    for (let y = 2020; y >= 1970; y--) {
+        years.push(y)
+    }
+
     return (
-        <div className="absolute mt-4 py-2 w-44 bg-dark rounded-2xl shadow-xl font-bold text-center justify-center">
-            <DropdownItem onSwitch={props.onSwitch} year={2020}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2019}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2018}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2017}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2016}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2015}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2014}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2013}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2012}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2011}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2010}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2009}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2008}/>
-            <DropdownItem onSwitch={props.onSwitch} year={2007}/>
+        <div className="absolute mt-4 py-2 w-44 bg-dark rounded-2xl shadow-xl font-bold text-center justify-center h-96 overflow-auto">
+            {years.map((year) => {
+                return (
+                    <DropdownItem
+                        onSwitch={props.onSwitch}
+                        year={year}
+                    />
+                )
+            })}
         </div>
     )
 }
