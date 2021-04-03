989f481ba5d28767f3b23d4dd39fe90dfb1d32fc
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-04-03 17:56:23 -0400

    Install simplebar-react which is a nicer looking scrollbar for internal page components

--- client/src/components/DropdownItem.jsx
+++ client/src/components/DropdownItem.jsx
@@ -1,6 +1,6 @@
 const DropdownItem = ({ year, onSwitch }) => {
     return (
-        <button className="rounded-md px-4 py-1 text-white w-16 transition duration-500 ease-in-out hover:bg-dark-1 shadow-md focus:outline-none focus:bg-dark-1" 
+        <button className="rounded-md px-3 py-1 text-white w-16 transition duration-500 ease-in-out hover:bg-dark-1 shadow-md focus:outline-none focus:bg-dark-1" 
         onClick={() => onSwitch(year)}>
             {year}
         </button>
--- client/src/components/DropdownList.jsx
+++ client/src/components/DropdownList.jsx
@@ -1,4 +1,6 @@
 import DropdownItem from "../components/DropdownItem"
+import SimpleBarReact from "simplebar-react"
+import "simplebar/src/simplebar.css"
 
 const DropdownList = (props) => {
 
@@ -9,15 +11,17 @@ const DropdownList = (props) => {
     }
 
     return (
-        <div className="absolute mt-4 py-2 w-44 bg-dark rounded-2xl shadow-xl font-bold text-center justify-center h-96 overflow-auto">
-            {years.map((year) => {
-                return (
-                    <DropdownItem
-                        onSwitch={props.onSwitch}
-                        year={year}
-                    />
-                )
-            })}
+        <div className="absolute mt-4 py-2 w-44 bg-dark rounded-2xl shadow-xl font-bold text-center justify-center">
+            <SimpleBarReact style={{ maxHeight: 250 }}>
+                {years.map((year) => {
+                    return (
+                        <DropdownItem
+                            onSwitch={props.onSwitch}
+                            year={year}
+                        />
+                    )
+                })}
+            </SimpleBarReact>
         </div>
     )
 }
