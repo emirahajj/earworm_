d3d4460ebe61d5788eb4155dadc8bafc44645659
AlfredoMora06 <agmmendez123@gmail.com>
2021-04-07 19:19:24 -0400

    fixes to artist-page

--- client/src/components/ArtistBio.jsx
+++ client/src/components/ArtistBio.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
-import logo from "../img/icon.png"
+
 
 const ArtistBio = (props) => {
     const [artistBio, setArtistBio] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
+
     let artist_name = props.name.replace(" ", "%20");
     const artist_url = "https://theaudiodb.p.rapidapi.com/search.php?s=" + artist_name;
     useEffect(() => {
         fetch(artist_url, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "PASS", //PASSWORD HERE!
+                "x-rapidapi-key": "3ed5717cfamsh84335cd3dde52fcp14b904jsn0282abb98094", //PASSWORD HERE!
                 "x-rapidapi-host": "theaudiodb.p.rapidapi.com"
             }
         })
@@ -26,12 +28,16 @@ const ArtistBio = (props) => {
 
 
     return (
-        <div>
-            {(artistBio ?
-                <p className="text-justify">{artistBio}</p>
-                :
-                <p> Bio Not Available</p>
-            )}
+        <div className="collapsible">
+            <button className="toggle" onClick={() => setIsOpen(!isOpen)}>Toggle</button>
+            {isOpen &&
+                <div className="content">
+                    {(artistBio ?
+                        <p className="text-justify">{artistBio}</p>
+                        :
+                        <p> Bio Not Available</p>
+                    )}
+                </div>}
         </div>
     );
 
--- client/src/components/ArtistsThumbImg.jsx
+++ client/src/components/ArtistsThumbImg.jsx
@@ -9,7 +9,7 @@ const ArtistsThumbImg = (props) => {
         fetch(artist_url, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "PASS", //PASSWORD HERE!
+                "x-rapidapi-key": "3ed5717cfamsh84335cd3dde52fcp14b904jsn0282abb98094", //PASSWORD HERE!
                 "x-rapidapi-host": "theaudiodb.p.rapidapi.com"
             }
         })
@@ -30,7 +30,7 @@ const ArtistsThumbImg = (props) => {
             {(artistImg ?
                 <img src={artistImg} alt="N/A"></img>
                 :
-                <img src={logo} alt="Album Cover"></img>
+                <img className="m-auto" src={logo} alt="Album Cover"></img>
             )}
         </div>
     );
