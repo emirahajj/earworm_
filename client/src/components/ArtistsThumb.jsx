b1bfb7284c8c5307c19d67a1cdbc20d8db98e6c0
emira <emirahajj@gmail.com>
2021-04-07 20:11:10 -0400

    small artist route typo

--- client/src/components/ArtistsThumb.jsx
+++ client/src/components/ArtistsThumb.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const ArtistsThumb = (props) => {
     return (
-        <Link to={"/artists/" + props.name}>
+        <Link to={"/artists/" + props.name.replace(' ', '-')}>
             <div className="flex justify-start items-center ml-5 mb-8">
                 <div className="flex-none w-24 h-24 rounded-2xl overflow-hidden shadow-md ml-12">
                     <ArtistsThumbImg name={props.name.replace(' ', '-')} />
