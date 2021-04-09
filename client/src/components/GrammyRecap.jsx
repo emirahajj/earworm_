10bbb47b43bcaa16bfe7d916da6a00b342f36409
emira <emirahajj@gmail.com>
2021-04-09 16:04:48 -0400

    grammyrecap component styling

--- client/src/components/GrammyRecap.jsx
+++ client/src/components/GrammyRecap.jsx
@@ -2,9 +2,9 @@ import GrammyComponent from '../components/GrammyComponent'
 
 const GrammyRecap = (props) => {
     return (
-        <div className="flex flex-row mt-10 mb-10">
+        <div className="flex flex-row justify-center  bg-opacity-90 rounded-3xl bg-dark-1 mt-10 mb-10">
         {props.awards.length > 0 ?  
-            (<div>
+            (<div className="my-2">
                 <h1 className="font-bold text-center text-3xl">Awards</h1>
                 <div className="flex flex-row my-4 justify-center">
                     {props.awards.map(element => {
@@ -14,7 +14,7 @@ const GrammyRecap = (props) => {
                 <p className="mb-4 text-center font-thin">This album won {props.awards.length} Grammy's at the {props.awards[0].year} Grammy's</p>
                 {props.awards.map((awardObj)=>{
                     return <div>
-                            <h1 className=" bg-dark-1 text-center mr-5 py-1 px-4 my-2 rounded-2xl w-full shadow-md">{awardObj.award}</h1>
+                            <h1 className=" bg-gray-2 text-center mr-5 py-1 px-4 my-2 rounded-2xl w-full shadow-md">{awardObj.award}</h1>
                             </div>
                 })}
             </div>) : <p></p>
