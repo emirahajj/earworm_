2297062f6444f7b90e3e4c51b189f1924decdd75
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-04-12 14:01:46 -0400

    Add scale transition on hover in Genres page

--- client/src/components/GenreThumb.jsx
+++ client/src/components/GenreThumb.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 const  GenreThumb = (props) => {
     return(
         <Link to ={"/genres/" + props.genre}>
-            <div className="bg-dark w-40 h-40 text-center rounded-full lg:w-48 lg:h-48">           
+            <div className="bg-dark w-40 h-40 text-center rounded-full lg:w-48 lg:h-48 transition duration-500 ease-in-out transform hover:scale-110">           
                 {/* <img src={logo} className=  "w-40 h-40 rounded-full" alt="genre-image"/> */}
                 <h1 className="flex justify-center text-md font-bold my-10 pt-16 lg:text-xl lg:pt-20">{props.genre}</h1>
             </div>
