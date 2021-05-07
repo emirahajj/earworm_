4c77ba8735307ba4be346ae736cc05635539adc0
Shah Isha Redohy <blackeyes0043@gmail.com>
2021-05-07 00:23:45 -0400

    Autocomplete SearchBox

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import NavItem from "./NavItem"
 import { Link } from "react-router-dom"
+import SearchBox from "./searchBox"
 
 const Navbar = () => {
     return (
         <nav>
-            <ul className="flex px-10 py-4 space-x-8 font-bold">
-                <Link to="/home"><NavItem text="Home" /></Link>
-                <Link to="/artists"><NavItem text="Artists" /></Link>
-                <Link to="/genres"><NavItem text="Genres" /></Link>
-                <Link to="/about"><NavItem text="About" /></Link>
+            <ul className="flex justify-between font-bold">
+                <div className="flex px-10 py-4 space-x-8 ">
+                    <Link to="/home"><NavItem text="Home" /></Link>
+                    <Link to="/artists"><NavItem text="Artists" /></Link>
+                    <Link to="/genres"><NavItem text="Genres" /></Link>
+                    <Link to="/about"><NavItem text="About" /></Link>
+                </div>
+                <div>
+                    <SearchBox className="flex justify-start"/>
+                </div>
             </ul>
         </nav>
     )
--- client/src/pages/Artists.jsx
+++ client/src/pages/Artists.jsx
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Navbar from "../components/Navbar"
-import { fetchArtistsLetter } from '../api/index';
+import { fetchArtistsLetter, fetchArtist } from '../api/index';
 import "../App.css";
 import ArtistsThumb from "../components/ArtistsThumb";
-import LoadingRing from "../components/LoadingRing"
+import LoadingRing from "../components/LoadingRing";
 
 const Artists = () => {
     const [letters, setLetters] = useState('A');
     const [artists, setArtists] = useState(null);
-    const [searchLetters, setSearchLetters] = useState('');
 
+
+    //*********latest edition******* */
+    // const [display, setDisplay] = useState("false");                 //For clicking the box and outside of it/when to display the results
+    // const [storedArtists, setStoredArtists] = useState([]);         //Holder array for API artists data return
+    // const [suggestedArtists, setSuggestedArtists] = useState([]);   //Array that would hold the results that would be displayed.
+    // const [wrapperRef] = useRef(null);                              //wrapper
+    //*******"What does the future hodl?" - Elon Musk */
     var highlight = true;
 
 
@@ -22,6 +28,21 @@ const Artists = () => {
         });
     }, [letters])
 
+
+    // useEffect(() => {
+    //     fetchArtist(storedArtists).then((response) => {
+    //         setStoredArtists(response.data)                                          //All the values fetched from the APi is stored here.
+    //     }); 
+        
+    //     const artistObjects = new Array(14).fill().map((v,i) => storedArtists[i]);  //artistObjects is a new array of 12 that holds elements of storedArtists. 
+   
+    //          console.log(artistObjects);
+    //          //sconsole.log(autoArtists);
+    //          //console.log(storedArtists);
+    // }, [])
+
+    console.log(letters);
+
     const onLetterChange = (letter) => {
         if (letter !== letters) {
             setLetters(letter);
@@ -32,17 +53,15 @@ const Artists = () => {
     return (
         <div>
             <Navbar />
-
-            <input
+            {/* <input
                 className="flex mx-auto w-1/4 mt-14 mb-4 p-4 bg-search"
                 type="text"
                 placeholder="Search Artist"
                 onChange={(e) => {
                     setSearchLetters(e.target.value); //value of searchbar
-                    //console.log(searchLetters);
+                    console.log(searchLetters);
                 }}
-            />
-
+            /> */}
             <div className="flex justify-center fade-in pb-6">
                 <h2 className="flex justify-center text-center bg-dark w-3/5 h-14 mt-10 pt-4 rounded-full font-bold">Browse through some of your favorite artists: </h2>
             </div>
@@ -66,13 +85,7 @@ const Artists = () => {
                     <div className="flex justify-center">
                         <div className="justify-center container grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 fade-in">
                             {
-                                artists.filter((val) => {
-                                    if (searchLetters == "") {
-                                        return val.name
-                                    } else if (val.name.toLowerCase().includes(searchLetters.toLowerCase())) {
-                                        return val.name
-                                    }
-                                }).map((artist, index) => {
+                                artists.map((artist, index) => {
                                     return <ArtistsThumb key={index} name={artist.name} id={artist._id} />;
                                 })
                             }
