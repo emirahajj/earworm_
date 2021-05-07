1b65cb1117131deeff87bde212d83a604a623c2a
Shah Isha Redohy <blackeyes0043@gmail.com>
2021-05-07 01:00:14 -0400

    Little problems that I had with Alfredo's PR is solved

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <ul className="flex justify-between font-bold">
                 <div className="flex px-10 py-4 space-x-8 ">
                     <Link to="/home"><NavItem text="Home" /></Link>
-                    <Link to="/artists"><NavItem text="Artists" /></Link>
+                    <Link to="/artists/A"><NavItem text="Artists" /></Link>
                     <Link to="/genres"><NavItem text="Genres" /></Link>
                     <Link to="/about"><NavItem text="About" /></Link>
                 </div>
--- client/src/components/SuggestedList.jsx
+++ client/src/components/SuggestedList.jsx
@@ -5,7 +5,7 @@ const SuggestedList = (props) => {
     return (
         <div>
             <div className="flex justify-start p-4 bg-dark  border-b border-white rounded">
-                <Link to={"/artists/" + props.name.replace(' ', '-')}>
+                <Link to={"/artist/" + props.name.replace(' ', '-')}>
                     <p className="mt-4 text-lg font-bold">{props.name}</p>
                 </Link>
             </div>
